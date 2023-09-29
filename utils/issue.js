7451634fc4c2fad76e09a428f0edcd1684c8b02d
yhj-zone <59201908+yhj-zone@users.noreply.github.com>
2023-09-29 12:06:39 +0800

    chore: update token

--- utils/issue.js
+++ utils/issue.js
@@ -1,41 +1,50 @@
-const { Octokit } = require("@octokit/core");
+const { createAppAuth } = require("@octokit/auth-app");
+const { Octokit } = require("@octokit/rest");
 
 const octokit = new Octokit({
+  // authStrategy: createAppAuth,
   auth: process.env.TOKEN,
+  request: {
+    fetch: fetch,
+  },
 });
 
-const open = async ({ owner, repo, title, body }) => {
-  try {
-    console.log("opening issue");
-    const res = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+const open = async ({owner, repo, title, body}) => {
+  try {    
+    console.log('opening issue');
+    const res = await octokit.request("POST /repos/{owner}/{repo}/issues",{
       owner,
       repo,
       title,
       body,
     });
-    console.log("opened");
+    console.log('opened');
     return res;
   } catch (error) {
     console.log(error);
     throw error;
   }
-};
+}
 
-const lock = async ({ owner, repo, issueNumber }) => {
-  console.log("locking issue");
+const lock = async ({owner, repo, issueNumber}) => {
+  console.log('locking issue');
   await octokit.request(
-    "PUT /repos/{owner}/{repo}/issues/{issue_number}/lock",
-    {
-      owner: owner,
-      repo: repo,
-      issue_number: issueNumber,
-      lock_reason: "resolved",
-    }
-  );
-  console.log("locked");
-};
+    "PUT /repos/{owner}/{repo}/issues/{issue_number}/lock",{
+    owner: owner,
+    repo: repo,
+    issue_number: issueNumber,
+    lock_reason: 'resolved'
+  });
+  console.log('locked');
+}
 
 module.exports = {
   open,
   lock,
-};
+}
+
+// lock({
+//   owner: 'headllines',
+//   repo: 'hackernews-daily',
+//   issueNumber: 39,
+// });
